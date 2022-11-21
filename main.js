b386732c5b0b38f9fb002bd6042b4e00b838b8e8
GIGIPR3 <gigiprencipe47@gmail.com>
2022-11-22 00:12:12 +0100

    .

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 var app = new Vue({
         el: '#root',
         data: {
-            array: [],
-            emailRandom: '',
+            array: []
+            /* emailRandom: '', */
         },
         methods: {
             genera() {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                    .then((response=>{
-                        for(let i = 0; i < 10; i++){
-                            let emailRandom = response.data.response
-                            console.log(emailRandom)
-                        }
-                    }))
+                    .then((response) => {
+                        let emailRandom = response.data.response;
+                        console.log(emailRandom);
+                        this.array.push(emailRandom);
+                    })
+
             }
         }
     },
